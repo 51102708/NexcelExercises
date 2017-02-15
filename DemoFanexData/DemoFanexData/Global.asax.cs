c6756956b63890fb45c7a9490e39e0406d882e63
Raymond Le <raymond.le@nexcel.vn>
2017-02-15 17:57:43 +0700

    add DemoFanexData

---
+++ DemoFanexData/DemoFanexData/Global.asax.cs
@@ -0,0 +1,26 @@
+﻿using Fanex.Data;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace DemoFanexData
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            DbSettingProviderManager
+              .StartNewSession()
+              .UseDefaultConnectionStringProvider()
+              .UseDefaultDbSettingProvider(
+                resourcePath: "~/App_Data/northwindSP.xml",
+                ignoreRedundantParameters: false,
+                enableWatching: true)
+              .Run();
+        }
+    }
+}
